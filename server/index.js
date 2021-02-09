e2412d6680a2b877b775c94dd81779ba2780a4d7
Colin Chauche <colin.chauche@gmail.com>
2021-02-08 19:35:02 -0500

    Add basic express server with static file serving

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 "use strict";
+const app = require('./server.js');
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 "use strict";
+const express = require('express');
+const path = require('path');
+const app = express()
+
+app.use(express.static(path.join(__dirname, '../public/')));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+module.exports = app;

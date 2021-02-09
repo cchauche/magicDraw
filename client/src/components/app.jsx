e0d5f374e9c24641c59957586113188ac3257954
Colin Chauche <colin.chauche@gmail.com>
2021-02-09 16:08:47 -0500

    Add state for App and created TOOL_TYPES constant

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import PropertiesPanel from "./PropertiesPanel";
 import ToolBar from "./ToolBar";
 import DrawingArea from './DrawingArea';
+import { TOOL_TYPES } from "./shapes/constants";
 
-function App() {
-  return (
-  <div id="app">
-    <ToolBar />
-    <DrawingArea />
-    <PropertiesPanel />
-  </div>)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shapes: {},
+      toolMode: TOOL_TYPES.SEL,
+      selected: {},
+      isDrawing: false,
+    }
+  }
+
+  render () {
+    return (
+      <div id="app">
+        <ToolBar />
+        <DrawingArea />
+        <PropertiesPanel />
+      </div>)
+  }
 }
 
 export default App;
---
+++ client/src/components/shapes/constants.js
@@ -0,0 +1,6 @@
+export const TOOL_TYPES = {
+    RECT: 'rect',
+    ELLI: 'ellipse',
+    TEXT: 'text',
+    SEL: 'select'
+}

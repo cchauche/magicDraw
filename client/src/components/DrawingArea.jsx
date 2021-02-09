7bb5df73eb9207590be58daadc5a4726801f4633
Colin Chauche <colin.chauche@gmail.com>
2021-02-09 14:07:45 -0500

    Basic layout with canvas complete

--- client/src/components/DrawingArea.jsx
+++ client/src/components/DrawingArea.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Layer, Stage } from "react-konva";
 
 const DrawingArea = () => {
   return (
-    <div id="drawing">
-      This is the drawing area
-    </div>
+    <main id="drawing">
+      <Stage
+      width={window.innerWidth - 450}
+      height={(window.innerWidth - 450) * (22/17)}
+      >
+      </Stage>
+    </main>
   )
 }
 

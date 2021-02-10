bec0b207003d4e0b274b3dae1dbd32d24a17651a
Colin Chauche <colin.chauche@gmail.com>
2021-02-09 21:50:39 -0500

    Brain storm helper functions

---
+++ client/src/components/helpers/shapeHelpers.js
@@ -0,0 +1,7 @@
+import { TOOL_TYPES } from "../shapes/constants";
+
+//todo: Function to create a rectangle shape
+
+//todo: Function to create an ellipse shape
+
+//todo: function to create a text box

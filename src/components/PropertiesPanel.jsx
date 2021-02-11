2159d8e73d434a6975af51dd9e7c9f4b95636fd3
Colin Chauche <colin.chauche@gmail.com>
2021-02-10 23:38:55 -0500

    Add properties to panel

--- src/components/PropertiesPanel.jsx
+++ src/components/PropertiesPanel.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useCallback } from "react";
 import { useStore } from "./helpers/state";
 
+const getSelectedShape = (state) => state.shapes[state.selected];
+
 const PropertiesPanel = () => {
+  const selectedShape = useStore(getSelectedShape);
+
+  const updateAttr = useCallback((event) => {
+    const attr = event.target.name;
+
+    updateAttribute(attr, event.target.value);
+  }, []);
+
   return (
     <aside className="panel">
       <h4>Properties</h4>
-      <p></p>
-    </aside>
-  )
-}
+      <div className="properties">
+        {selectedShape ? (
+          <>
+            <div className="key">
+              Type <span className="value">{selectedShape.type}</span>
+            </div>
 
-export default PropertiesPanel
+            <div className="key">
+              Stroke{" "}
+              <input
+                className="value"
+                name="stroke"
+                type="color"
+                value={selectedShape.stroke}
+                onChange={updateAttr}
+              />
+            </div>
+
+            <div className="key">
+              Fill{" "}
+              <input
+                className="value"
+                name="fill"
+                type="color"
+                value={selectedShape.fill}
+                onChange={updateAttr}
+              />
+            </div>
+          </>
+        ) : (
+          <div className="no-data">Nothing Selected</div>
+        )}
+      </div>
+    </aside>
+  );
+};
+
+export default PropertiesPanel;
